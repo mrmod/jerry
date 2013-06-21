e78e047c4205eeb68f9a6a2d637625155a0725a9
Bruce <mcmoranbjr@gmail.com>
2013-06-20 23:07:20 -0700

    Implemented controller side additions for multiple node runs

--- jerry.rb
+++ jerry.rb
@@ -102,11 +102,8 @@ class RacecarDriver
     mco_action(config_file, 'details', collective,opts << node)
   end
   # Do a puppet run
-  def run_node(node, config_file, collective, opts=DEFAULT_OPTS.dup)
+  def run_node(config_file, collective, opts=DEFAULT_OPTS.dup)
     @logger.debug("Running #{__method__} for #{node}")
-    opts << '-I'
-    opts << node
-    opts << 'runonce'
     mco_action(config_file, 'puppetrun',collective, opts)
   end
 end
@@ -114,6 +111,29 @@ end
 
 # Strong as any man alive
 # Config file is by default jerry.yaml in the same dir as jerry.rb
+# = Helpers
+# == discovery_nodes
+# Discover nodes using mco's ping plugin
+# [collective]
+#  [String] collective
+# [opts]
+#  [Array] opts Options array
+# == node_details
+# Get node details
+# [node]
+#  [String] node Node fqdn
+# [collective]
+#  [String] collective
+# [opts]
+#  [Array] opts Options array
+# == run_node
+# Run a node using the puppetd agent
+# [node]
+#  [String] node Node fqdn
+# [collective]
+#  [String] collective
+# [opts]
+#  [Array] opts Options array
 class Jerry < Sinatra::Base
   register Sinatra::ConfigFile
   config_file(File.join(File.dirname(__FILE__), 'jerry.yaml'))
@@ -122,18 +142,13 @@ class Jerry < Sinatra::Base
 
   DEFAULT_OPTIONS = ['--dt',settings.mco_settings['discover_time'], '-t' , settings.mco_settings['timeout']]
   DEFAULT_COLLECTIVE = settings.collective
- 
   before do 
     @racecar = RacecarDriver.new(settings.whitelist)
     @mco_config = settings.mco_config
     @logger = Logger.new(settings.logfile)
     @logger.level = settings.log_level
   end
-
   helpers do
-    # Find stuff
-    # @param [String] collective
-    # @param [Array] opts Options array
     def discover_nodes(collective = DEFAULT_COLLECTIVE, opts = DEFAULT_OPTIONS.dup)
       collective = DEFAULT_COLLECTIVE if collective.empty?
       @logger.debug("#{self.class}:#{__method__}: Collective: #{collective}, opts: #{opts.inspect}")
@@ -149,37 +164,28 @@ class Jerry < Sinatra::Base
         @node_count = @statistics = @nodes = 0
       end
     end
-    # Get node details
-    # @param [String] node Node FQDN
-    # @param [String] collective
-    # @param [Array] opts Options array
+
+    
     def node_details(node, collective = DEFAULT_COLLECTIVE, opts = DEFAULT_OPTIONS.dup)
       @logger.debug("Getting node details for #{node} in #{collective}, opts: #{opts.inspect}")
       @racecar.node_details(node,@mco_config, collective, opts)
     end
 
-    # Run a node using the puppetd agent
-    # @param [String] node Node fqdn
-    # @param [String] collective
-    # @param [Array] opts Options array
-    def run_node(node, collective = DEFAULT_COLLECTIVE, opts = DEFAULT_OPTIONS.dup)
+    def run_node(collective = DEFAULT_COLLECTIVE, opts = DEFAULT_OPTIONS.dup)
       @logger.debug("Starting run for #{node}")
-      @racecar.run_node(node,@mco_config,collective,opts)
+      @racecar.run_node(@mco_config,collective,opts)
     end
-
   end
 	# Here.I.Am
 	get '/' do
 		@choices = ['Authorize','Discover','Inventory', 'Run']
 		haml :index
 	end
-
 	# Authorize index
   get '/authorize' do
   	@authorized_nodes = @racecar.authorized_nodes
   	haml :authorize
   end
-
   # Authorized nodes list
   get '/authorize/nodes' do
     @authorized_nodes = @racecar.authorized_nodes
@@ -187,7 +193,6 @@ class Jerry < Sinatra::Base
      # match pattern could be here
     haml :authorize_results, :layout => false
   end
-
   # Authorize a given node
   post '/authorize' do
     node = params[:node]
@@ -207,7 +212,6 @@ class Jerry < Sinatra::Base
     @logger.debug("Displaying #{@authorized_nodes.inspect} ")
     haml :authorize_results, :layout => false
   end
-
   # Delete an authorized node
   # @todo Delete verb?
   post '/authorize/delete/:node' do
@@ -216,12 +220,10 @@ class Jerry < Sinatra::Base
     @authorized_nodes = @racecar.authorized_nodes
     haml :authorize_results, :layout => false
   end
-
   # Discover index
   get '/discover' do
     haml :discover
   end
-
   # Discover results
   post '/discover/results' do
     @logger.debug("Calling discover_nodes with #{params[:collective]}")
@@ -234,21 +236,17 @@ class Jerry < Sinatra::Base
     discover_nodes
     haml :discovery_results, :layout => false
   end
-  
-
   # Inventory index
   get '/inventory' do
   	@inventory = "Inventory"
   	haml :inventory
   end
-
   # Inventory a node
   post '/inventory' do
     @inventory = node_details(params[:node])
     @logger.debug("Model provided: #{@inventory.inspect}")
     haml :inventory_results, :layout => false
   end
-
   # Run index
   get '/run' do
     haml :run
@@ -259,10 +257,20 @@ class Jerry < Sinatra::Base
     @node = params[:node]
     @collective = params[:collective]
     @logger.debug("Run request for #{@node}@#{@collective}")
-    @node_run = run_node(@node, @collective)
+    opts = DEFAULT_OPTIONS.dup
+    case params[:node_type]
+      when 'regex'
+        opts = opts + ["-F","fqdn='@node'","runonce"]
+      when 'fact'
+        opts = opts + ["-F", @node, "runonce"]
+      when 'options'
+        opts =  @node.split(' ')
+      else
+        opts = opts + ['-I', @node, 'runonce']
+      end
+    @node_run = run_node(@collective, opts.flatten)
     haml :run_results, :layout => false
   end
-
   # Run a specific node for curl-based scripts
   get '/run/:collective/:node' do
     @node = params[:node]
